63ed57792883457ad1626428c8f1a3473c1d9728
Shawn <shawntennity@gmail.com>
2019-11-05 09:28:40 -0500

    Updates

---
+++ precache-manifest.1d735350f7ec4835fa39abbb5f11f9e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee411f45facfa5a1fb152efa858d7e3",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "281de96718f0b5bcbd02",
+    "url": "/react-monsters-rolodex/static/css/main.2ac2ab4b.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/react-monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "281de96718f0b5bcbd02",
+    "url": "/react-monsters-rolodex/static/js/main.65e6b0b7.chunk.js"
+  },
+  {
+    "revision": "e3713dfbf0885b83451f",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.f9329b93.js"
+  }
+]);
